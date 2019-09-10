5c8e617fd767a1baa0ee871f25b28433e9cbaae5
Nicholas Garcia <ngarcia158@west-mec.org>
2019-09-10 13:30:32 -0700

    s

--- js/snake.js
+++ js/snake.js
@@ -121,7 +121,7 @@ SNAKE.Snake = SNAKE.Snake || (function() {
             rowShift = [-1, 0, 1, 0],
             xPosShift = [],
             yPosShift = [],
-            snakeSpeed = 2,
+            snakeSpeed = 10,
             isDead = false,
             isPaused = false;
 
